e8dbb1d9b75b7acd2b0c57282e17cfa39b722927
Maciek <maciejpietranik@gmail.com>
2017-11-19 19:26:55 +0100

    v2 - dodana opcja sharowania postow przez email, dodany system komentarzy, numerowanie stron

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'djangodevmaciek@gmail.com'
+EMAIL_HOST_PASSWORD = 'django123'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
